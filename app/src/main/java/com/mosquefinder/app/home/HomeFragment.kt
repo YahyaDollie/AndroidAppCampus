bf35fc1bcecd488f797078e653383a82c2a0f424
Yahya <yahya@entersekt.com>
2021-10-27 13:58:44 +0200

    Added BottomSheet menu

--- app/src/main/java/com/mosquefinder/app/home/HomeFragment.kt
+++ app/src/main/java/com/mosquefinder/app/home/HomeFragment.kt
@@ -7,14 +7,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.mosquefinder.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mosquefinder.app.network.VolleyRequest
 import java.util.*
@@ -25,9 +24,9 @@ Main fragment for the home view
 
 class HomeFragment : Fragment(), DailyTimesCallbackListener, CurrentTimeCallbackListener {
     private lateinit var homeView: View
-    private lateinit var mapButton: FloatingActionButton
-    private lateinit var mosqueButton: FloatingActionButton
-    private lateinit var calendarButton: FloatingActionButton
+    private lateinit var mapButton: ImageButton
+    private lateinit var mosqueButton: ImageButton
+    private lateinit var calendarButton: ImageButton
     private lateinit var navController: NavController
     private lateinit var volleyRequest: VolleyRequest
     private lateinit var fajrTime:String
@@ -40,6 +39,9 @@ class HomeFragment : Fragment(), DailyTimesCallbackListener, CurrentTimeCallback
     private lateinit var broadcastHandler: BroadcastHandler
     private lateinit var homeNavigator: HomeNavigator
     private lateinit var spinnerDropdown: Spinner
+    private lateinit var btn_show:ImageButton
+    private lateinit var bottomSheetView:View
+    private lateinit var bottomSheet : BottomSheetDialog
 
     val intentFilter = IntentFilter(Intent.ACTION_TIME_TICK)
 
@@ -72,8 +74,19 @@ class HomeFragment : Fragment(), DailyTimesCallbackListener, CurrentTimeCallback
         asr_time = homeView.findViewById(R.id.asr_time)
         magrieb_time = homeView.findViewById(R.id.magrieb_time)
         ishai_time = homeView.findViewById(R.id.ishai_time)
+        btn_show = homeView.findViewById(R.id.bottom_sheet_menu_btn)
+        btn_show.setOnClickListener {
+            bottomSheet = BottomSheetDialog(homeView.context, R.style.BottomSheetMenuTheme)
+            bottomSheetView = LayoutInflater.from(homeView.context).inflate(
+                R.layout.bottom_sheet_menu,
+                homeView.findViewById(R.id.bottom_sheet_menu)
+            )
+            handleNavigation()
+            bottomSheet.setContentView(bottomSheetView)
+            bottomSheet.show()
+        }
         registerReciver()
-        handleNavigation()
+//        handleNavigation()
     }
 
     override fun onStart() {
@@ -101,17 +114,20 @@ class HomeFragment : Fragment(), DailyTimesCallbackListener, CurrentTimeCallback
     }
 
     private fun handleNavigation(){
-        mosqueButton = homeView.findViewById(R.id.mosque_button)
+        mosqueButton = bottomSheetView.findViewById(R.id.mosque_btn)
         mosqueButton.setOnClickListener {
             this.navController.navigate(R.id.action_homeFragment_to_mosqueFind)
+            bottomSheet.dismiss()
         }
-        mapButton = homeView.findViewById(R.id.maps_button)
+        mapButton = bottomSheetView.findViewById(R.id.maps_btn)
         mapButton.setOnClickListener {
             this.navController.navigate(R.id.action_home_fragment_to_mapsFragment)
+            bottomSheet.dismiss()
         }
-        calendarButton = homeView.findViewById(R.id.calendar_button)
+        calendarButton = bottomSheetView.findViewById(R.id.calendar_btn)
         calendarButton.setOnClickListener {
             this.navController.navigate(R.id.action_home_fragment_to_calendar)
+            bottomSheet.dismiss()
         }
     }
 
